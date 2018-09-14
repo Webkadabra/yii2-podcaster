9331939fd276c0b0d61d997459b683dc4e1c9c80
Sergii Gama <balls@sergiigama.com>
2018-09-15 02:09:15 +0300

    Update copyright

--- src/podcaster/Module.php
+++ src/podcaster/Module.php
@@ -2,10 +2,8 @@
 /**
  * Podcast Engine, <https://webkadabra.github.io/yii2-podcast-engine/>
  *
- * Copyright (C) 2017-present Sergii Gamaiunov <devkadabra@gmail.com>
+ * Copyright (C) 2017-present Sergii Webkadabra <devkadabra@gmail.com>
  * All rights reserved.
- *
- * THIS IS NOT A FREE SOFTWARE. PLEASE, VISIT <https://webkadabra.github.io/yii2-podcast-engine/> FOR DETAILS
  */
 namespace webkadabra\podcaster;
 
